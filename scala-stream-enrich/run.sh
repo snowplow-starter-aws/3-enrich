c3c9687990679d923803f79a3202583f5b13238e
Peter Siemen <peter.siemen@googlemail.com>
2020-10-27 19:05:16 +0100

    latest

---
+++ scala-stream-enrich/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+docker run -it  -e AWS_ACCESS_KEY_ID=AKIAROCUQNR56QNDY2MV -e AWS_SECRET_ACCESS_KEY=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  petersiemen/scala-stream-enrich:test
+
+
+
+
